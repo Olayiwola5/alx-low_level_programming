c91738fa2065a2635847217aa2a487a3c8030396
Garuba Abdulrasheed <abdulrasheedolayiwola5@gmail.com>
2022-10-18 18:38:05 -0700

    nested_loop

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,17 @@
  */
 int main(void)
 {
-	int n, sum;
+	int n;
+	int sum = 0;
 
-	while (n < 1024)
+	for (n = 0; n < 1024; n++)
 	{
-		if ((n % 3) == 0 || (n % 5) == 0)
+		if (n % 3 == 0 || n % 5 == 0)
 		{
 			sum += n;
 		}
-		n++;
 	}
 	printf("%d\n", sum);
 	return (0);
 }
+
