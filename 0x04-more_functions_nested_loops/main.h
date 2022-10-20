69b52d4965a2a9d53391ef7c9bc6ff9d8a28ec05
Garuba Abdulrasheed <abdulrasheedolayiwola5@gmail.com>
2022-10-20 06:23:38 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,10 @@
 #ifndef MAIN_H
-int convert_day(int month, int day);
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int yea
-		r);
+void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
@@ -14,6 +12,7 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 int _isupper(int c);
 int mul(int a, int b);
+int convert_day(int month, int day);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
