99fb9b217a87d2c4d352680853fc25f3f3506e49
Garuba Abdulrasheed <abdulrasheedolayiwola5@gmail.com>
2022-10-24 15:21:10 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 void reset_to_98(int *n);
-id swap_int(int *a, int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -12,7 +12,10 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+
+
+
 
 
 #endif
+
