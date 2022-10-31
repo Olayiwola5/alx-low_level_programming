fa56f37d7369a82c6fd422abfbaf03da2253b724
Garuba Abdulrasheed <abdulrasheedolayiwola5@gmail.com>
2022-10-31 12:55:14 -0700

    0x06. C - More pointers, arrays and strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
