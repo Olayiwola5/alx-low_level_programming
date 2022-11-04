b63d5ca51e1a6018b430504eb1fd58c37d8b461d
Garuba Abdulrasheed <abdulrasheedolayiwola5@gmail.com>
2022-11-04 13:14:25 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Int representing a character
+ * Return: 0 on success
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
