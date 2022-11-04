5a874517523eb6825579eca477ec58b682917335
Garuba Abdulrasheed <abdulrasheedolayiwola5@gmail.com>
2022-11-04 05:13:05 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,24 +1,15 @@
-#include "holberton.h"
 #include <stdio.h>
 
 /**
- * main - program that prints its name
- * @argc: argument count
- * @argv: argunment vector
- *
- * Return: int
+ * main - a program that prints its name, followed by a new line
+ * @argc: arguement count
+ * @argv: argument value, a strings that come after calling function
+ * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	int count;
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 
-	for (count = 0; count < argc; count++)
-	{
-		if (argc == 1)
-			printf("%s\n", argv[count]);
-		else
-			break;
-	}
 	return (0);
 }
