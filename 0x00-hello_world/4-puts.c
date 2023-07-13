5c502da772ac00360e724a0734e37eba1ef7c01d
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-07-13 08:02:28 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - a C program that prints exactly a given string with a new line
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	puts("\n");
+	return (0);
+}
