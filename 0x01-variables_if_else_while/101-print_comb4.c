03b595d54a7f3a8a20c0838c5a174deca72eabe6
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-07-14 08:25:52 -0700

    0x01. C - Variables, if, else, while tasks

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,43 +3,52 @@
   * main - Entry point
   * written by Olayiwola
   *
-  * Description - Write a program that prints all possible different combinations
+  * Description - Write a program that prints
+  * all possible different combinations
   * of three digits.Numbers must be separated by ,, followed by a space
   * The three digits must be different
-  * 012, 120, 102, 021, 201, 210 are considered the same combination 
-  * of the three digits 0, 1 and 2Print only the smallest combination of three digits
+  * 012, 120, 102, 021, 201, 210 are considered
+  * the same combination
+  * of the three digits 0, 1 and 2
+  * Print only the smallest combination of three digits
   *
   * Return: 0 (success)
 */
 int main(void)
 {
-	int  x;
-	int y;
-	int z;
+	int digit1 = 0;
+	int digit2, digit3;
 
-	for (x = 0; x < 99; x++)
+	while (digit1 <= 9)
 	{
-		for (y = 0; y < 99; y++)
+		digit2 = 0;
+		while (digit2 <= 9)
 		{
-			for (z = 0; z < 99; z++)
+			digit3 = 0;
+			while (digit3 <= 9)
 			{
-				if (x != y &&
-				    x < y &&
-				    y != z &&
-				    y < z)
+				if (digit1 != digit2 &&
+				    digit1 < digit2 &&
+				    digit2 != digit3 &&
+				    digit2 < digit3)
 				{
-					putchar(x + 48);
-					putchar(y + 48);
-					putchar(z + 48);
-					if ( z != 99)
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
+
+					if (digit1 + digit2 + digit3 != 24)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
+				++digit3;
 			}
+			++digit2;
 		}
+		++digit1;
 	}
 	putchar('\n');
+
 	return (0);
 }
