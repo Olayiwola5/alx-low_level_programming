139adf564b0b3f1aee8111f85d8053c6dbd73800
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-07-18 18:54:36 -0700

    corrections on erring tasks

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,72 +1,32 @@
-#include"main.h"
+#include <stdio.h>
 
 /**
- * numLength - returns the length of string
- *
- * @num: operand number
- *
- * Return: number of digits
-*/
-
-int numLength(int num)
-{
-	int length = 0;
-
-	if (!num)
-		return (1);
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-	return (length);
-}
-
-/**
- * main - Entry point
- *
- * Description: prints the first 98 Fibonacci numbers
- *	starting with 1 and 2 followed by a new line
- *
- *	Solution was copied from Nobert Patrick
- *	Wise, github handle: Trikcode
- *
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line
  * Return: Always 0 (Success)
-*/
+ */
 
 int main(void)
 {
-	int count, initial0s;
-	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+	int i;
+	unsigned long int j, k, next, sum;
 
-	for (count = 1; count <= 98; ++count)
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
 	{
-		if (f1o > 0)
-			printf("%lu", f1o);
-		initial0s = numLength(mx) - 1 - numLength(f1);
-
-		while (f1o > 0 && initial0s > 0)
+		if (j < 4000000 && (j % 2) == 0)
 		{
-			printf("%d", 0);
-			--initial0s;
+			sum = sum + j;
 		}
-
-		printf("%lu", f1);
-
-		sum = (f1 + f2) % mx;
-		sumo = f1o + f2o + (f1 + f2) / mx;
-		f1 = f2;
-		f1o = f2o;
-		f2 = sum;
-		f2o = sumo;
-
-		if (count != 98)
-			printf(", ");
-		else
-			printf("\n");
+		next = j + k;
+		j = k;
+		k = next;
 	}
+
+	printf("%lu\n", sum);
 
 	return (0);
 }
