a3189325cd48b4cdd5d29f9a51f28d0d45e65e26
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-07-24 13:48:44 -0700

    corrections on 0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,14 +15,14 @@ char *_strcpy(char *dest, char *src)
 
 	i = 0;
 	len = 0;
-	while (*(src + i) != '\0')
+	while (src[len] != '\0')
 	{
-		++len;
+		len++;
 	}
 	for (i = 0; i < len; i++)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 	}
-	*(dest + i) = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
