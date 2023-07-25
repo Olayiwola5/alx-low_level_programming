93ebce576c0eb5c4147391df4bfb5c8f8499c26e
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-07-24 23:31:55 -0700

    corrections on 0x05-pointer tasks

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,14 +14,7 @@ void puts_half(char *str)
 	len = 0;
 	while (str[len] != '\0')
 		len++;
-	if (len % 2 == 0)
-	{
-		len / 2;
-	{
-	else
-	{
-		(len - 1) / 2;
-	}
+	n = (len % 2 == 0) ? len / 2 :(len - 1) / 2;
 	for (i = n; i < len; i++)
 	{
 		putchar(str[i]);
