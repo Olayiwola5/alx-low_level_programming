779c72dbd35cb7083015eb9e74fd22581d37fae9
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-08-07 09:31:56 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,10 +17,11 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	//for (i = 0; s1[i] != '\0'; i++)
-	//	;
-	//for (j = 0; s2[j] != '\0'; j++)
-	//	;
+	/*for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	*/
 
 	str = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
 	if (str == NULL)
