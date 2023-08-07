6e2ef9165970f59333510f9b44f78b8ed208c861
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-08-07 16:41:16 -0700

    hard thing

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,7 +10,7 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *str;
+	char *str, *tempo;
 	int total_len;
 	int i;
 
@@ -21,13 +21,16 @@ char *argstostr(int ac, char **av)
 	total_len = 0;
 	for (i = 0; i < ac; i++)
 	{
-		total_len = strlen(av[i]);
+		tempo = av[i];
+
 	}
-	total_len++;
+	tota_len = 0;
+	while (tempo[i] != '\0')
+		total_len++;
 	str = malloc(total_len * sizeof(str) + 12);
 	if (str == NULL)
 		return (NULL);
-	for (i = 1; i < ac; i++)
+	for (i = 1; i < total_len; i++)
 	{
 		strcat(str, av[i]);
 		strcat(str, "\n");
