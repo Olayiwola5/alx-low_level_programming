0799368eec4897cd39148e4ea8c3c253fdc18a3d
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-08-07 17:27:26 -0700

    hard

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,22 +13,21 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, length;
+	int i, j, k, len;
 	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	/*find length of vector + '\0' which makes it a 2d array*/
-	length = 0;
+	len = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
-			length++;
-		length++;
+			len++;
+		len++;
 	}
 
-	str = malloc((length + 1) * sizeof(char));
+	str = malloc((len + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
 
