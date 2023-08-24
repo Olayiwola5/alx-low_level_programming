4adcd4133bac52425295566918f7cb92e096be5d
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-08-24 06:30:16 -0700

    first linkedlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,10 +12,11 @@ size_t print_list(const list_t *h)
 	size_t i;
 	
 	i = 0;
-	while (h == NULL)
+	while (h != NULL)
 	{
-		printf("[0] (nil)\n");
-		if (h->str != NULL)
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
 		{
 			printf("[%u] %s\n", h->len, h->str);
 			h = h->next;
