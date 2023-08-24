c7dbfb283fa521634bc9a15d1cfbb3a8234aebef
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-08-24 11:41:01 -0700

    tasks

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,21 +1,18 @@
 #include <string.h>
 #include "lists.h"
-
 /**
 * free_list - frees a singly linked list.
 * @head: a linked list to print
 *
 * Return: void.
 */
-
 void free_list(list_t *head)
 {
 	/* creates a temporary nod */
 	list_t *temp;
 
-	while (head)
+	while (temp = head != NULL)
 	{
-		temp = head;
 		head = head->next;
 		free(temp);
 		free(temp->next);
