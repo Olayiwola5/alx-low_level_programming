1605508e48ee651d6aa891477123fb4fd9c50c62
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-08-24 09:56:21 -0700

    task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,32 +7,34 @@
  * @str: string to store in the list.
  * Return: address of the head.
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *n_node, *curr_node;
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		;
+	size_t n;
 
 	n_node = malloc(sizeof(list_t));
 	if (n_node == NULL)
 		return (NULL);
 
-	n_node->len = i;
 	n_node->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+		;
+
+	n_node->len = n;
 	n_node->next = NULL;
-	curr_node =  *head;
+	curr_node = *head;
+
 	if (curr_node == NULL)
-		*head = curr_node;
+	{
+		*head = n_node;
+	}
 	else
 	{
 		while (curr_node->next != NULL)
-		{
-			curr_node =  curr_node->next;
-		}
+			curr_node = curr_node->next;
 		curr_node->next = n_node;
 	}
+
 	return (*head);
 }
