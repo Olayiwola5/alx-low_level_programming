2116edb9fdd00a7cc0b3483907444e20ae71c577
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-08-28 16:22:32 -0700

    free linkedlist

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - frees a linked list
+ * @head: head of the list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *del_node;
+
+	while (*head)
+	{
+		del_node = *head;
+		*head = *head->next;
+		free(del_node);
+	}
+}
