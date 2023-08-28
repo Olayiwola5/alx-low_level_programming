99fc4e79db4ee9d227e235ecbdc3e015640d0477
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-08-28 16:05:40 -0700

    error founded{

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,43 +11,26 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *last, *temp;
 
 	last = malloc(sizeof(listint_t));
-	if (last == NULL)
+	if (last != NULL)
 	{
 		last->n = n;
 		last->next = NULL;
 	}
-	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
+	else
 		return (NULL);
 
+	//temp = malloc(sizeof(listint_t));
 	temp = *head;
-	if (*head == NULL)
+	if (temp == NULL)
 		*head = last;
 	else
 	{
 		while (temp->next != NULL)
 			temp = temp->next;
+		temp->next = last;
 	}
-	temp->next = last;
 	/* update head */
 	*head = last;
 
 	return (last);
-}
-#include "lists.h"
-int main(void)
-{
-    listint_t *head;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    return (0);
 }
