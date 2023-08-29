c106e14f4849ad6714ec0180bba16af648f62c03
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-08-29 04:24:50 -0700

    task5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,14 +6,12 @@
 *
 * Return: void.
 */
-
 void free_listint2(listint_t **head)
 {
-	listint_t *del_node; /* Create a temporary node */
+	listint_t *del_node;
 
 	if (head == NULL)
 		return;
-
 	while (*head)
 	{
 		del_node = (*head)->next;
