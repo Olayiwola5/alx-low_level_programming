c3c2a44c84cd74b3b0b9742c6b0655922f4e445e
Olayiwola5 <abdulrasheedolayiwola5@gmail.com>
2023-08-29 04:40:29 -0700

    task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* pop_listint - delete a new node at the beginning of a singly linked list.
+* @head: a linked list to print
+*
+* Return: the address of the new element, or NULL if it failed.
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int num;
+
+	if (*head == NULL)
+		return (0);
+	node = *head;
+	*head = node->next;
+	num = node->n;
+	free(node);
+
+	return (num);
+}
